a96832a081c807e52be0cc42da6d955e9ec7e3bb
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-03-06 22:02:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 int cbinsearch(int *arr, int size, int value) {
   int count = 0;
     for (int a = 0; a <= size; a++) {
-        if (*(arr + i) == value)
+        if (*(arr + a) == value)
             count += 1;
     }
     return count;
