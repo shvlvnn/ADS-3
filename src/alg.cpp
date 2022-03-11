e606eb824c23cab55e3b06188947cb6013d1bc28
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-03-11 11:42:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
-    for (int a = 0; a <= size; a++) {
-        if (*(arr + a) == value)
-            count += 1;
+    int x = 0, y = size - 1, count = 0;
+    int z, f;
+    while (x <= y) {
+        z = (y + x) / 2;
+        if (arr[z] == value) {
+            count++;
+            f = z;
+            break;
+        }
+    if (value < arr[z])
+        y = z - 1;
+    else
+        x = z + 1;
     }
+    if (f >= 0) {
+        for (int a = 0; a < f; a++) {
+            if (arr[a] == value)
+        count++;
+    }
+    for (int b = size - 1; b > f; b--) {
+        if (arr[b] == value)
+        count++;
+    }
+    }
+  if (count > 0)
     return count;
-  return 0;
+  else
+    return 0;
 }
